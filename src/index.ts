9c13295786111558bec67625c1b296af714361f3
tinywaves <dhzhme@gmail.com>
2025-08-17 21:53:57 +0800

    feat: use koa to start a server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export function sum(a: number, b: number) {
-  return a + b;
-}
+import process from 'node:process';
+import Koa from 'koa';
+
+const app = new Koa();
+
+const port = Number(process.env.PORT) || 8080;
+const hostname = process.env.HOSTNAME || '0.0.0.0';
+
+app.listen(port, hostname, () => {
+  console.info(`Server running at http://${hostname}:${port}`);
+});
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,3 @@
-import { expect, it } from 'vitest';
-import { sum } from '../src';
+import { it } from 'vitest';
 
-it('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+it('', () => {});
